ce9fd7a682a657dbc57e8d798a424a60683997ec
manisha <manisha.verma23@gmail.com>
2022-03-28 06:49:25 +0530

    testing

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,7 +7,7 @@ nvm install node
 
 #create out working directory if it doesn't exist
 
-DIR= "/home/ec2-user/nodejs-app"
+DIR="/home/ec2-user/nodejs-app"
 if [-d "$DIR"]; then
   echo "${DIR} exists"
 else
