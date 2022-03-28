1334bd7d8a859e0a27ee6d85edbff0b71b470c80
manisha <manisha.verma23@gmail.com>
2022-03-28 11:12:14 +0530

    updated restart script

---
+++ scripts/application_restart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#stopping existing node servers
+echo "Restart any existing node servers"
+npm install nodemon
+nodemon app.js
+
--- scripts/application_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#stopping existing node servers
-# echo "stopping any existing node servers"
-# pkill node
